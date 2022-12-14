1eb31ddc563643ab77baf22d38131e2a60924029
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-12-13 17:51:27 -0800

    Added text ui to the npc interaction box

--- Assets/Scripts/InteractionTrigger.cs
+++ Assets/Scripts/InteractionTrigger.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractionTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Text that shows up when player enteres interaction box
+    public GameObject uiInteractionText;
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player")
             {
                 Debug.Log("Player has entered trigger");
+                uiInteractionText.SetActive(true);
                 // Player has entered the trigger volume
                 // Do something here, such as displaying a message or triggering an event
             }
@@ -17,6 +21,7 @@ public class InteractionTrigger : MonoBehaviour
         if (other.gameObject.tag == "Player")
             {
                 Debug.Log("Player has left trigger");
+                uiInteractionText.SetActive(false);
                 // Player has entered the trigger volume
                 // Do something here, such as displaying a message or triggering an event
             }
