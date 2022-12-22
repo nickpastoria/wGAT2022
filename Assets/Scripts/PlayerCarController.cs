ed744b601c7aa8afe41a4756d57c9ff3e045e7ba
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-12-21 22:39:08 -0800

    UI animation for car swerving
    Since I'm still waiting on some of the basic elements of the game to be made I created a small script that visually rotates the UI car elements to make the scene more dynamic

--- Assets/Scripts/PlayerCarController.cs
+++ Assets/Scripts/PlayerCarController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerCarController : MonoBehaviour
 {
+        enum Direction 
+    {
+        Left,
+        Right
+    };
     // Start is called before the first frame update
     public GameObject[] Lanes;
     public int startingLane = 1;
@@ -13,6 +18,10 @@ public class PlayerCarController : MonoBehaviour
     //Things Needed For Smooth Damp
     public float smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
+
+    private Direction lastKeyPress; 
+
+
 
     void Start() {
         currentLane = startingLane;
@@ -25,8 +34,16 @@ public class PlayerCarController : MonoBehaviour
     }
 
     void InputHandler() {
-        if(Input.GetAxisRaw("Horizontal") > 0.1f && Input.GetButtonDown("Horizontal")) currentLane++;
-        if(Input.GetAxisRaw("Horizontal") < -0.1f && Input.GetButtonDown("Horizontal")) currentLane--;
+        if(Input.GetAxisRaw("Horizontal") > 0.1f && Input.GetButtonDown("Horizontal"))
+        {
+            currentLane++;
+            lastKeyPress = Direction.Right;
+        } 
+        if(Input.GetAxisRaw("Horizontal") < -0.1f && Input.GetButtonDown("Horizontal"))
+        {
+            currentLane--;
+            lastKeyPress = Direction.Left;
+        } 
         currentLane = Mathf.Clamp(currentLane, 0, Lanes.GetLength(0) - 1);
     }
 
@@ -34,5 +51,17 @@ public class PlayerCarController : MonoBehaviour
     {
             // Use Mathf.Lerp to interpolate between the start and end positions
         transform.position = Vector3.SmoothDamp(transform.position, Lanes[currentLane].GetComponent<Transform>().position, ref velocity, smoothTime);
+    }
+
+    public float getVelocity(){
+        // This will be an amalgamation of magnitude and direction inferred from two things.
+        // The magnitude will come from the velocity magnitude and the direction will come from the last keypress
+        float carDir = 0;
+        float vel1D;
+
+        if(lastKeyPress == Direction.Right) carDir = 1.0f;
+        if(lastKeyPress == Direction.Left) carDir = -1.0f;
+        vel1D = velocity.magnitude * carDir;
+        return vel1D;        
     }
 }
