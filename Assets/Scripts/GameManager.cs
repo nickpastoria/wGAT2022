53384cd62b072b3ed7698d8d00083ddd2954edfd
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-12-22 23:28:59 -0800

    Made spawner work for song bpm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public AudioSource gameSong;
     public int songBPM = 110;
