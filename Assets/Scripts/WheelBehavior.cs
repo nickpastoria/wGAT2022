53c731afbaaf726f9bca37ed38d6e76970cc2be7
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-12-26 01:14:24 -0800

    Wheel Bobbing
    Another case of nick adding early polish!
    I know I'm not supposed to do that but it's so tempting

--- Assets/Scripts/WheelBehavior.cs
+++ Assets/Scripts/WheelBehavior.cs
@@ -11,22 +11,13 @@ public class WheelBehavior : MonoBehaviour
     public float damping = 0.5f;
     public float velocityToRotationScale = 1.0f;
 
-    private RectTransform rectTransform;
-
     private float currentAngle;
     private float currentVelocity;
-
-    void Start()
-    {
-        // Get UI element attatched to gameobject at start of game
-        rectTransform = gameObject.GetComponent<RectTransform>();
-    }
-
     // Update is called once per frame
     void Update()
     {
         float targetAngle = Mathf.Clamp(player.getVelocity()*velocityToRotationScale, -maxDeg, maxDeg);
         currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentVelocity, damping, rotationSpeed);
-        rectTransform.Rotate(Vector3.forward, currentAngle - rectTransform.rotation.eulerAngles.z);
+        transform.Rotate(Vector3.forward, currentAngle - transform.rotation.eulerAngles.z);
     }
 }
