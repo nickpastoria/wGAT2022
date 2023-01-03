ef53e6b2be2420d3dd191a7e7acf6208985139f7
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2023-01-02 22:43:05 -0800

    Created set_Carspeed template file
    It don't work yet

---
+++ Assets/Scripts/Effects/effect_SetCarSpeed.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect_SetCarSpeed : Effects
+{
+    public float newRoadSpeed;
+
+    public float timeToNewSpeed;
+
+    public override void RunEffect()
+    {
+
+    }
+    
+}
