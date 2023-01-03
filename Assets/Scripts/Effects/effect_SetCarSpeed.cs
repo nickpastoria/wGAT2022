eb55b1ba6d958864e9669982b99d8efdd3444cf7
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2023-01-02 23:45:25 -0800

    Set car speed works

--- Assets/Scripts/Effects/effect_SetCarSpeed.cs
+++ Assets/Scripts/Effects/effect_SetCarSpeed.cs
@@ -7,10 +7,21 @@ public class effect_SetCarSpeed : Effects
     public float newRoadSpeed;
 
     public float timeToNewSpeed;
+    private float current = 0f;
+    private float currentVelocity = 0f;
+    private bool isDampening = false;
 
     public override void RunEffect()
     {
-
+        isDampening = true;
+    }
+    
+    private void Update()
+    {
+        if(!isDampening) return;
+        current = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().roadSpeed;
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().roadSpeed = Mathf.SmoothDamp(current, newRoadSpeed, ref currentVelocity, timeToNewSpeed);
+        if(current == newRoadSpeed) isDampening = false;
     }
     
 }
