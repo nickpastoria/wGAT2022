259ea9f8a0ea891865f83c790d8125d4970a21c6
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2023-01-06 20:26:17 -0800

    FIxed bug for fadout of startup volume

--- Assets/FadeTransition.cs
+++ Assets/FadeTransition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FadeTransition : MonoBehaviour
 {
 // Start is called before the first frame update
+    [SerializeField]
     private Animator animator;
 
     void Awake()
